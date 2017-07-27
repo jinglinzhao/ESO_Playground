9c287cf3f15cb789746cdd906b09d4a8a5e565b9
jinglinzhao <jzhao@durufle.phys.unsw.edu.au>
2017-07-27 17:56:53 +1000

    Remove BERV
    hdulist[0].header['HIERARCH ESO DRS BERV'].

--- ESO_HARPS.py
+++ ESO_HARPS.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jun  8 16:57:03 2017
+Created on Thu Jul 27 16:43:04 2017
 
 @author: jzhao
 """
@@ -13,9 +13,7 @@ Created on Thu Jun  8 16:57:03 2017
 ###########
 # Updates #
 ###########
-# bc = hdulist[0].header['HIERARCH ESO DRS BERV'] @26/07/17
-
-#############################################
+# Remove hdulist[0].header['HIERARCH ESO DRS BERV']. 
 
 import sys
 from astropy.io import fits
@@ -37,15 +35,16 @@ def rms(num):
     return sqrt(sum(n*n for n in num)/len(num))
 #############################################
 
-FILE        = glob.glob('../ccf_fits/*fits')
+FILE        = glob.glob('../HD224817/3-ccf_fits/*fits')
 N           = len(FILE)
 N_start     = 0
 N_end       = N
 n_file      = N_end - N_start
 MJD         = np.zeros(n_file)
 RV_g        = np.zeros(n_file)
+RV_HARPS    = np.zeros(n_file)
 
-x           = np.arange(-10, -1+0.1, 0.1)                                # over sampling to 0.1 km/s [-10.2, -0.8]
+x           = np.arange(-20, -2+0.1, 0.1)                                # over sampling to 0.1 km/s [-10.2, -0.8]
 y           = np.zeros(len(x))
 
 plt.figure()
@@ -59,9 +58,10 @@ for n in range(N_start, N_end):
 
     
     hdulist     = fits.open(FILE[n])
-    bc          = hdulist[0].header['HIERARCH ESO DRS BERV'] / 1000             # barycentric RV, originally in m/s
-    v0          = hdulist[0].header['CRVAL1'] - bc                              # velocity on the left (N_starting point)
-    # v0          = hdulist[0].header['CRVAL1']  
+    v0          = hdulist[0].header['CRVAL1']                                   # velocity on the left (N_starting point)
+    RV_HARPS[n] = hdulist[0].header['HIERARCH ESO DRS CCF RVC']                 # Baryc RV (drift corrected) (km/s)
+    if RV_HARPS[n] > -10:
+        continue
     MJD[n]      = hdulist[0].header['MJD-OBS']
     
     CCF         = hdulist[0].data                                               # ccf 2-d array
@@ -69,21 +69,21 @@ for n in range(N_start, N_end):
     delta_v     = hdulist[0].header['CDELT1']                                   # velocity grid size 
     v           = v0 + np.arange(CCF.shape[1]) * delta_v                        # velocity array (whole range)
 
-    if n == 0:                                                                  # not used
-        idx_min     = ccf == min(ccf)
-        v_min       = v[idx_min]
-
     f           = CubicSpline(v, ccf / ccf.max())
     y           = f(x)
-    popt, pcov  = curve_fit( gaussian, x, y, [-1.76, -5.467, 2.5, 1])
-#    print((popt[1] - v_min)*1000)
+    popt, pcov  = curve_fit( gaussian, x, y, [-1.76, RV_HARPS[n], 2.5, 1])
+    # plt.plot(x, y, x, gaussian(x, *popt))
     RV_g[n]     = popt[1]
     x_new       = x
     y_new       = (y - popt[3]) / popt[0]
     # plt.plot(x_new, y_new, '-')
     
-    writefile   = '../ccf_dat/ccf' + str(n) + '.dat'
+    writefile   = '../HD224817/4-ccf_dat/ccf' + str(n) + '.dat'
     np.savetxt(writefile, y_new)
 
-plt.plot(MJD, (RV_g - np.mean(RV_g))*1000, '.')    
+# Verification # 
+idx = (RV_g == 0);
+plt.plot( RV_g[~idx] *1000, RV_HARPS[~idx] * 1000, '+')
+# plt.plot(MJD[~idx], RV_g[~idx] *1000, '.', MJD[~idx], RV_HARPS[~idx] * 1000, '+')
+#plt.plot(MJD, (RV_g - np.mean(RV_g))*1000, '.', MJD, (RV_HARPS - np.mean(RV_HARPS)) * 1000, '+')
 plt.show()
