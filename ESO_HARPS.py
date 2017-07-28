1ca945b990075a9c2faa625e3834c97d3f14796c
jinglinzhao <jzhao@durufle.phys.unsw.edu.au>
2017-07-28 14:26:15 +1000

    Update to work with HD96700

--- ESO_HARPS.py
+++ ESO_HARPS.py
@@ -15,7 +15,7 @@ Created on Thu Jul 27 16:43:04 2017
 ###########
 # Remove hdulist[0].header['HIERARCH ESO DRS BERV']. 
 
-import os
+# import os
 import sys
 import shutil
 from astropy.io import fits
@@ -37,7 +37,8 @@ def rms(num):
     return sqrt(sum(n*n for n in num)/len(num))
 #############################################
 
-FILE        = glob.glob('../HD224817/3-ccf_fits/*fits')
+star        = 'HD96700'
+FILE        = glob.glob('../' + star + '/3-ccf_fits/*fits')
 N           = len(FILE)
 N_start     = 0
 N_end       = N
@@ -46,7 +47,7 @@ MJD         = np.zeros(n_file)
 RV_g        = np.zeros(n_file)
 RV_HARPS    = np.zeros(n_file)
 
-x           = np.arange(-20, -2+0.1, 0.1)                                       # over sampling to 0.1 km/s [-10.2, -0.8]
+x           = np.arange(12.5-10, 12.5+10+0.1, 0.1)                                       # over sampling to 0.1 km/s [-10.2, -0.8]
 y           = np.zeros(len(x))
 
 plt.figure()
@@ -61,15 +62,21 @@ for n in range(N_start, N_end):
     hdulist     = fits.open(FILE[n])
     v0          = hdulist[0].header['CRVAL1']                                   # velocity on the left (N_starting point)
     RV_HARPS[n] = hdulist[0].header['HIERARCH ESO DRS CCF RVC']                 # Baryc RV (drift corrected) (km/s)
-    if RV_HARPS[n] > -10:
-        shutil.move(FILE[n], '../HD224817/3-ccf_fits/abandoned/')
+    v_noise     = hdulist[0].header['HIERARCH ESO DRS CCF NOISE'] * 1000        # RV_noise in m/s
+    if v_noise > 5:
+        shutil.move(FILE[n], '../' + star + '/3-ccf_fits/abandoned/')
         continue
+    
+#    if RV_HARPS[n] > -10:
+#        shutil.move(FILE[n], '../' + star + '/3-ccf_fits/abandoned/')
+#        continue
     MJD[n]      = hdulist[0].header['MJD-OBS']
     
     CCF         = hdulist[0].data                                               # ccf 2-d array
     ccf         = CCF[- 1, :]                                                   # ccf 1-d array (whole range)
     delta_v     = hdulist[0].header['CDELT1']                                   # velocity grid size 
     v           = v0 + np.arange(CCF.shape[1]) * delta_v                        # velocity array (whole range)
+    # plt.plot(v,ccf)
 
     f           = CubicSpline(v, ccf / ccf.max())
     y           = f(x)
@@ -78,7 +85,7 @@ for n in range(N_start, N_end):
     RV_g[n]     = popt[1]
     x_new       = x
     y_new       = (y - popt[3]) / popt[0]
-    # plt.plot(x_new, y_new, '-')
+    plt.plot(x_new, y_new, '-')
     
     output_name = FILE[n]
     output_name = output_name.replace('3-ccf_fits', '4-ccf_dat')
@@ -86,8 +93,9 @@ for n in range(N_start, N_end):
     np.savetxt(writefile, y_new)
 
 # Verification # 
-idx = (RV_g == 0);
-plt.plot( RV_g[~idx] *1000, RV_HARPS[~idx] * 1000, '+')
-# plt.plot(MJD[~idx], RV_g[~idx] *1000, '.', MJD[~idx], RV_HARPS[~idx] * 1000, '+')
-#plt.plot(MJD, (RV_g - np.mean(RV_g))*1000, '.', MJD, (RV_HARPS - np.mean(RV_HARPS)) * 1000, '+')
-plt.show()
+if 0:
+    idx = (RV_g == 0);
+    plt.plot( RV_g[~idx] *1000, RV_HARPS[~idx] * 1000, '+')
+    plt.plot(MJD[~idx], RV_g[~idx] *1000, '.', MJD[~idx], RV_HARPS[~idx] * 1000, '+')
+    plt.plot(MJD, (RV_g - np.mean(RV_g))*1000, '.', MJD, (RV_HARPS - np.mean(RV_HARPS)) * 1000, '+')
+    plt.show()
