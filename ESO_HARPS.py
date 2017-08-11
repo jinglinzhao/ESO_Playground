2dd8f22b51885b49250a14d018d68ddafce7d352
jinglinzhao <jzhao@durufle.phys.unsw.edu.au>
2017-08-11 17:15:35 +1000

    # Updates #
    # simplify n_file @08/08/17
    # Add a loop to calculate RVC and RVW @08/08/17

--- ESO_HARPS.py
+++ ESO_HARPS.py
@@ -14,6 +14,10 @@ Created on Thu Jul 27 16:43:04 2017
 # Updates #
 ###########
 # Remove hdulist[0].header['HIERARCH ESO DRS BERV']. 
+# simplify n_file @08/08/17
+# Add a loop to calculate RVC and RVW @08/08/17
+
+#############################################
 
 # import os
 import sys
@@ -25,65 +29,72 @@ import numpy as np
 from scipy.optimize import curve_fit
 from scipy.interpolate import CubicSpline
 import math
+from statistics import median
+
+#############################################
 
 def gaussian(x, a, mu, sigma, C):
     val = a / ( (2*math.pi)**0.5 * sigma ) * np.exp(-(x - mu)**2 / sigma**2) + C
     return val
 
-# Root mean square
-#def rms(num):
-#    return sqrt(sum(n*n for n in num)/len(num))
 #############################################
 
-star        = 'Gl581'
-FILE        = glob.glob('../' + star + '/3-ccf_fits/*fits')
-N           = len(FILE)
-N_start     = 0
-N_end       = N
-n_file      = N_end - N_start
+STAR        = 'HD189733'
+FILE        = glob.glob('../' + STAR + '/3-ccf_fits/*fits')
+n_file      = len(FILE)
 MJD         = np.zeros(n_file)
 RV_g        = np.zeros(n_file)
 RV_HARPS    = np.zeros(n_file)
+FWHM_HARPS  = np.zeros(n_file)
 
-# x           = np.arange(12.5-10, 12.5+10+0.1, 0.1)                            # HD96700 FWHM = 6.9      # over sampling to 0.1 km/s [-10.2, -0.8]
-# x           = np.arange(5.5-18, 5.5+18+0.1, 0.1)                                # HD142 FWHM = 15
-# x           = np.arange(31-10, 31+10+0.1, 0.1)                                  # CoRot-7
-x           = np.arange(-9.2-4.5, -9.2+4.5+0.1, 0.1) 
-y           = np.zeros(len(x))
+for n in range(n_file):
+    # progress bar #
+    sys.stdout.write('\r')
+    sys.stdout.write("[%-50s] %d%%" % ('='*int((n+1)*50./n_file), int((n+1)*100./n_file)))
+    sys.stdout.flush()    
+    
+    hdulist     	= fits.open(FILE[n])
+    RV_HARPS[n] 	= hdulist[0].header['HIERARCH ESO DRS CCF RVC']                 # Baryc RV (drift corrected) (km/s)
+    FWHM_HARPS[n] = hdulist[0].header['HIERARCH ESO DRS CCF FWHM']
+
+print('\n')    
+
+RVC = median(RV_HARPS)
+RVW = median(FWHM_HARPS) * 1.5
+x   = np.arange(RVC-RVW, RVC+RVW+0.1, 0.1)
+y   = np.zeros(len(x))
 
 plt.figure()
 
-for n in range(N_start, N_end):
+for n in range(n_file):
     
     # progress bar #
     sys.stdout.write('\r')
-    sys.stdout.write("[%-50s] %d%%" % ('='*int((n+1-N_start)*50./(N_end-N_start)), int((n+1-N_start)*100./(N_end-N_start))))
+    sys.stdout.write("[%-50s] %d%%" % ('='*int((n+1)*50./n_file), int((n+1)*100./n_file)))
     sys.stdout.flush()    
     
     hdulist     = fits.open(FILE[n])
-    v0          = hdulist[0].header['CRVAL1']                                   # velocity on the left (N_starting point)
+    v0          = hdulist[0].header['CRVAL1']                                   # velocity on the left (N_STARting point)
     RV_HARPS[n] = hdulist[0].header['HIERARCH ESO DRS CCF RVC']                 # Baryc RV (drift corrected) (km/s)
     v_noise     = hdulist[0].header['HIERARCH ESO DRS CCF NOISE'] * 1000        # RV_noise in m/s
-    star_read   = hdulist[0].header['OBJECT']
+    STAR_read   = hdulist[0].header['OBJECT']
     
     # remove file if necessary
-    if star_read != star:
-        print (' Achtung! ' + star_read + ' instead of '+ star)
-        shutil.move(FILE[n], '../' + star + '/3-ccf_fits/abandoned/')
+    if STAR_read != STAR:
+        print (' Achtung! ' + STAR_read + ' instead of '+ STAR)
+        shutil.move(FILE[n], '../' + STAR + '/3-ccf_fits/abandoned/')
         continue
     
     if v_noise > 5:
-        print(' Achtung! ' + star_read + ' too noisy')
-        shutil.move(FILE[n], '../' + star + '/3-ccf_fits/abandoned/')
+        print(' Achtung! ' + STAR_read + ' too noisy')
+        shutil.move(FILE[n], '../' + STAR + '/3-ccf_fits/abandoned/')
         continue
     
-    if (RV_HARPS[n] > -9.2+10) or (RV_HARPS[n] < -9.2-10):
-        print(' Achtung! ' +  star_read + ' largely offset')
-        shutil.move(FILE[n], '../' + star + '/3-ccf_fits/abandoned/')
+    if (RV_HARPS[n] > RVC+10) or (RV_HARPS[n] < RVC-10):
+        print(' Achtung! ' +  STAR_read + ' largely offset')
+        shutil.move(FILE[n], '../' + STAR + '/3-ccf_fits/abandoned/')
         continue
 
-    MJD[n]      = hdulist[0].header['MJD-OBS']
-    
     CCF         = hdulist[0].data                                               # ccf 2-d array
     ccf         = CCF[- 1, :]                                                   # ccf 1-d array (whole range)
     delta_v     = hdulist[0].header['CDELT1']                                   # velocity grid size 
@@ -92,14 +103,13 @@ for n in range(N_start, N_end):
 
     f           = CubicSpline(v, ccf / ccf.max())
     y           = f(x)
-    # popt, pcov  = curve_fit( gaussian, x, y, [-1.76, RV_HARPS[n], 2.5, 1])
-    popt, pcov  = curve_fit( gaussian, x, y, [-5, RV_HARPS[n], 4, 1])
+    popt, pcov  = curve_fit( gaussian, x, y, [-RVW/2, RV_HARPS[n], RVW/2, 1])
     # plt.plot(x, y, x, gaussian(x, *popt))
     RV_g[n]     = popt[1]
     
     if abs(RV_HARPS[n] - RV_g[n])*1000 > 5:
-        print(' Achtung! ' +  star_read + ' RV fitting issue')
-        shutil.move(FILE[n], '../' + star + '/3-ccf_fits/abandoned/')    
+        print(' Achtung! ' +  STAR_read + ' RV fitting issue')
+        shutil.move(FILE[n], '../' + STAR + '/3-ccf_fits/abandoned/')    
         continue
         
     x_new       = x
@@ -110,11 +120,18 @@ for n in range(N_start, N_end):
     output_name = output_name.replace('3-ccf_fits', '4-ccf_dat')
     writefile   = output_name.replace('.fits', '.dat')
     np.savetxt(writefile, y_new)
+    
+    MJD[n]      = hdulist[0].header['MJD-OBS']
+
+print('\n')        
+idx = (MJD == 0)
+np.savetxt('../' + STAR + '/info.dat', [RVC, RVW])
+np.savetxt('../' + STAR + '/MJD.dat', MJD[~idx])
+np.savetxt('../' + STAR + '/RV_HARPS.dat', RV_HARPS)
 
 # Verification # 
 if 0:
-    idx = (RV_g == 0)
     plt.plot( RV_g[~idx] *1000, RV_HARPS[~idx] * 1000, '+')
     plt.plot(MJD[~idx], RV_g[~idx] *1000, '.', MJD[~idx], RV_HARPS[~idx] * 1000, '+')
-    plt.plot(MJD, (RV_g - np.mean(RV_g))*1000, '.', MJD, (RV_HARPS - np.mean(RV_HARPS)) * 1000, '+')
+    plt.plot(MJD[~idx], (RV_g[~idx] - RV_HARPS[~idx]) * 1000, '.')
     plt.show()
