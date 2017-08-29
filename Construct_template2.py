69e2422430b5e6235b3bc1640019cfd681a7ac28
jinglinzhao <jzhao@durufle.phys.unsw.edu.au>
2017-08-29 15:58:28 +1000

    Modification
    modify to find out the 1st order coefficient

--- Construct_template2.py
+++ Construct_template2.py
@@ -8,7 +8,7 @@ Created on Tue Aug 22 15:01:51 2017
 
 
 import sys
-import shutil
+#import shutil
 from astropy.io import fits
 import glob
 import matplotlib.pyplot as plt
@@ -16,9 +16,10 @@ import numpy as np
 from scipy.optimize import curve_fit
 from scipy.interpolate import CubicSpline
 import math
-from math import  exp
+#from math import  exp
 from statistics import median
 from scipy.optimize import minimize
+from numpy.polynomial.hermite import hermfit
 
 #############################################
 def gaussian(x, a, mu, sigma, C):
@@ -34,11 +35,9 @@ def rms(num):
 STAR        = 'HD189733'
 FILE        = glob.glob('../' + STAR + '/3-ccf_fits/*fits')
 n_file      = len(FILE)
-MJD         = np.zeros(n_file)
-RV_fit      = np.zeros(n_file)
-RV_HARPS    = np.zeros(n_file)
-FWHM_HARPS  = np.zeros(n_file)
-ccf_max     = np.zeros(n_file)
+MJD, RV_fit, RV_HARPS, FWHM_HARPS, y_max    = np.zeros((5, n_file))
+LPD, A, S, K                                = np.zeros((4, n_file))
+array1      = np.zeros(n_file)
 
 for n in range(n_file):
     # progress bar #
@@ -52,17 +51,19 @@ for n in range(n_file):
 
 print('\n')  
 
+
+
 RVC     = median(RV_HARPS)
-RVW     = median(FWHM_HARPS) * 1.4
-x       = np.arange(RVC-RVW, RVC+RVW+0.1, 0.1)
+RVW     = median(FWHM_HARPS) * 1.5
+x       = np.arange(RVC-RVW, RVC+RVW, 0.1)
 y       = np.zeros(len(x))
-x_tmp   = np.arange(RVC-RVW, RVC+RVW+0.1, 0.1)
+x_tmp   = np.arange(RVC-RVW, RVC+RVW, 0.1)
 Y_tmp1  = np.loadtxt('../' + STAR + '/template1.dat')
-Y_tmp2  = np.arange(RVC-RVW, RVC+RVW+0.1, 0.1)
+Y_tmp2  = np.zeros(len(x_tmp))
 
 plt.figure()
 
-for n in range(n_file):    
+for n in range(n_file):
     
     # progress bar #
     sys.stdout.write('\r')
@@ -71,7 +72,6 @@ for n in range(n_file):
     
     hdulist     = fits.open(FILE[n])
     v0          = hdulist[0].header['CRVAL1']                                   # velocity on the left (N_STARting point)
-    RV_HARPS[n] = hdulist[0].header['HIERARCH ESO DRS CCF RVC']                 # Baryc RV (drift corrected) (km/s)
     MJD[n]      = hdulist[0].header['MJD-OBS']
     
     CCF         = hdulist[0].data                                               # ccf 2-d array
@@ -81,20 +81,51 @@ for n in range(n_file):
     v           = v0 + np.arange(CCF.shape[1]) * delta_v                        # velocity array (whole range)
     idx_v       = (v > RVC-RVW-0.1) & (v < RVC+RVW+0.1)
     x           = v[idx_v]
-    y           = ccf[idx_v] / max(ccf[idx_v])
-    popt, pcov  = curve_fit( gaussian, x, y, [-RVW/2, RV_HARPS[n], RVW/2, 1])
-    y           = (y - popt[3]) / popt[0]                                       # x is in original v grid; y is normalized
+    y           = ccf[idx_v] 
+    y_max[n]    = y.max()
+
+
+
+    # test begins
+    f_test      = CubicSpline(v, ccf)
+    y_test      = f_test(x_tmp)
+    y_test      = y_test/max(y_test)
+#    plt.figure(); plt.plot(x_tmp, y_test, x_tmp, Y_tmp1)
+    array_hermite = hermfit(x_tmp - RVC, y_test / Y_tmp1, 5)
+    array1[n]   = array_hermite[1]
     
+    
+    
+#    y           = ccf[idx_v] / max(ccf[idx_v])
+#    popt, pcov  = curve_fit( gaussian, x, y, [-RVW/2, RV_HARPS[n], RVW/2, 1])
+#    y           = (y - popt[3]) / popt[0]                                       # x is in original v grid; y is normalized
+
+        
     # Now shift the template to match the observation
-    
+    '''
     # Option 1: minimize rms
     def obj(x_shift):
-        f           = CubicSpline(x_tmp + x_shift, Y_tmp1)                      # shift the template to the right (when x_shift > 0) to match the observation
+        f           = CubicSpline(x_tmp + x_shift, Y_tmp1)                      # shift the template to the right (for x_shift > 0) to match the observation
         y_shift     = f(x)
-        return rms( (y_shift - y) * max(ccf[idx_v]) / ccf[idx_v] )
+        return rms( (y_shift * max(ccf[idx_v]) - y) / ccf[idx_v] )
     res     = minimize(obj, 0) 
     RV_fit[n] = res.x[0]
     
+    f           = CubicSpline( (x - res.x[0]), y )                              # shift the observed spectrum to the left (for x_shift > 0) in order to co-add to the template
+    y_tmp       = f(x_tmp)
+#    Y_tmp2       = Y_tmp2 + y_tmp
+    
+#    plt.figure()
+#    plt.plot(x_tmp - RVC, y_tmp / y.max() - Y_tmp2)
+    
+    obs         = y_tmp / y.max()
+    x_new       = x_tmp - RVC
+    LPD[n]      = sum((y_tmp / y.max() - Y_tmp2) * x_new)
+    sigma_2     = sum(obs * x_new**2) / sum(obs)
+    A[n]        = 1. / sigma_2**0.5 * sum(obs * x_new**1) / sum(obs)
+    S[n]        = 1. / sigma_2**1.5 * sum(obs * x_new**3) / sum(obs)
+    K[n]        = 1. / sigma_2**2.0 * sum(obs * x_new**4) / sum(obs)   
+    '''
     # Option 2: maximize exponential
 #    def obj(x_shift):
 #        f           = CubicSpline(x_tmp + x_shift, Y_tmp1)
@@ -104,7 +135,8 @@ for n in range(n_file):
 #    res     = minimize(obj, 1)
 #    RV_fit[n] = res.x[0]
         
-plt.plot(MJD, RV_fit, '.', MJD, RV_HARPS-RVC , '+')   
+#plt.plot(MJD, RV_fit, '.', MJD, RV_HARPS-RVC , '+')   
+Y_tmp2 = Y_tmp2 / sum(y_max)
     
 
 
